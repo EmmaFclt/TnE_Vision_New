9f94af71b062474a0ac3b576b5e81cc711114932
Emma Foucault <foucault.emma@gmail.com>
2020-02-11 19:39:55 +0100

    new routes created

--- app/javascript/components/filter.js
+++ app/javascript/components/filter.js
@@ -1,15 +1,10 @@
-import flatpickr from "flatpickr";
-// Date Filter
-
 const dateFilter = () => {
-  const dateTab = document.getElementById("date-filter");
-
-  dateTab.addEventListener("click", e => {
-    flatpickr("#date-filter");
-    console.log("ayo", dateTab);
+ const dateFilter = document.getElementById("date-filter");
+  dateFilter.addEventListener("click", (event) => {
+    event.currentTarget.innerText = 'Hold still...';
   });
 };
-export { dateFilter };
+export {dateFilter};
 
 const entityFilter = () => {
  const entityFilter = document.getElementById("entity-filter");
