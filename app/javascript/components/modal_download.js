ada3688380373e0377cde06ca2a01e79b372f3d2
Paula <paula.beltran.pb@gmail.com>
2020-02-15 17:43:58 +0100

    modalsdownload

---
+++ app/javascript/components/modal_download.js
@@ -0,0 +1,26 @@
+//var htmlToImage = require('html-to-image');
+
+
+
+
+//const download = () => {
+  //htmlToImage.toPng(document.querySelector('.modal-content'))
+  //.then(function (dataUrl) {
+    //debugger
+    //download(dataUrl, 'my-chart.png');
+
+//  });
+//}
+
+//const initDownloadModal = () => {
+  //if (document.querySelector('.modal-content')){
+    //const button = document.querySelector("#download-modal")
+      //  button.addEventListener("click", download)
+ // }
+
+
+//}
+
+
+
+//export {initDownloadModal}
