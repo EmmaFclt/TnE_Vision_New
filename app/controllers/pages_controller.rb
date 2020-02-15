d2c35d8fba3ea3dabbc6b923cf5ff1d467684937
Emma Foucault <foucault.emma@gmail.com>
2020-02-15 17:43:57 +0100

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -75,7 +75,64 @@ class PagesController < ApplicationController
 
   end
 
-  def favorite
-    raise
+  def addfavorite
+    current_user.favorites.include?(params[:graph]) ? current_user.favorites.delete(params[:graph]) : current_user.favorites << params[:graph]
+    current_user.save!
+    # raise
+    redirect_to :favorites
+  end
+
+  def favorites
+    @company = current_user.company
+    @transactions = @company.transactions
+    # @entities = []
+    # @transactions.each do |t|
+    #   @entities.push(t.entity) unless @entities.include?(t.entity)
+    # end
+
+    # @services = []
+    # @transactions.each do |t|
+    #   @services.push(t.service) unless @services.include?(t.service)
+    # end
+    @entities = @transactions.map{|t| t.entity}.reject{|entity| entity.nil?}.uniq
+
+    @services = @transactions.map{|t| t.service}.reject{|service| service.nil?}.uniq
+
+    if params[:filter]
+
+      if params[:filter][:compliancy] == '1'
+        @transactions = @transactions.where(compliancy: true)
+      end
+
+      if params[:filter][:entity]
+        @transactions = @transactions.where(entity: params[:filter][:entity])
+      end
+
+      if params[:filter][:service]
+        @transactions = @transactions.where(service: params[:filter][:service])
+      end
+
+      if params[:filter][:start_date]
+        @transactions = @transactions.where('start_date: >= params[:filter][:start_date]')
+      end
+    end
+
+    @types = @transactions.group_by { |t| t.transaction_type }
+                          .transform_values { |v| v.count }
+
+
+
+    @cities = @transactions.group_by{ |t| t.cities }
+                          .transform_values{|v| v.count }.sort_by{ |k, v| -v }[0..4].to_h
+
+    @transactions_count_week = @transactions.group_by_day_of_week(:start_date, format: "%a").count
+
+    ref = { '1' => 'January', '2' => 'February', '3' => 'March', '4' => 'April', '5' => 'May', '6' => 'June', '7'=> 'July', '8' => 'August', '9' => 'September', '10' => 'October', '11' => 'November', '12' => 'December' }
+    @company = current_user.company
+    @transactions = @company.transactions
+    amount_per_month = @transactions.group_by { |t| t.start_date.month }.transform_values { |v| v.pluck(:amount) }.transform_values(&:sum).sort_by {|k, v| k}
+    @sum_amount_per_month = amount_per_month.each_with_index { |a, i| a[1] = amount_per_month[i-1][1] + a[1] if i > 0 }.to_h.transform_keys(&:to_s).transform_keys{|k| ref[k]}
+
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
     get 'budget', to: 'pages#budget'
     get 'rse', to: 'pages#rse'
     get 'profil', to: 'pages#profil'
-    post 'favorite', to: 'pages#favorite'
+    get 'favorites', to: 'pages#favorites'
+    get 'addfavorite', to: 'pages#addfavorite'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :reports, only: %i[new create index destroy]
